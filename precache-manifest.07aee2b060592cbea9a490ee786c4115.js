be090d543790adb4f39d1b70e2b4b82bda450305
yevhenbilous <bilous.yevhen@gmail.com>
2020-01-23 12:26:37 -0600

    Updates

---
+++ precache-manifest.07aee2b060592cbea9a490ee786c4115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271783724086948616cb2c383651c23d",
+    "url": "/admin-panel/index.html"
+  },
+  {
+    "revision": "a2bc29cf23c038471872",
+    "url": "/admin-panel/static/js/2.ddad749a.chunk.js"
+  },
+  {
+    "revision": "3daf59f4d3df1af9bda656aad7f7d826",
+    "url": "/admin-panel/static/js/2.ddad749a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdd867353bee2780823c",
+    "url": "/admin-panel/static/js/main.445019ea.chunk.js"
+  },
+  {
+    "revision": "add5c68d22dccb0ee0f0",
+    "url": "/admin-panel/static/js/runtime-main.72524a3b.js"
+  }
+]);
